d4366ea6bcf0278656fd5e9a967729d80c96031c
Andrew <84246113+arstamm@users.noreply.github.com>
2025-01-30 22:52:26 -0700

    Added code to Program.kt

--- Classes/Program.kt
+++ Classes/Program.kt
@@ -1,1 +1,23 @@
 package classes
+
+// This is where the main program runs for the Event Planner
+class Program() {
+
+    // Input loop for Event Planner
+    fun run() {
+        println("")
+        println("------")
+        println("Event Planner")
+        println("------")
+        while (true) {
+            print("Enter option (enter \"exit\" to leave): ")
+            var response :String? = readLine()!!
+            if (response == "exit") {
+                println("Have a nice day!")
+                break
+            }
+            println(response)
+
+        }
+    }
+}
--- main.kt
+++ main.kt
@@ -1,12 +1,30 @@
-// Main to run the program
-
 package main
+import classes.Program
 
+// Main to run the program
 fun main() {
-    val file = File("Data/userdata.csv")
+    val program = Program()
+    program.run()
 }
 
+/***********************************************************************
 
+Commands for compiling and running all .kt files together in one project
+------------------------------------------------------------------------
+NOTE: ALL files must be error free in order for the compile command to work
+
+Mac OS (zsh):
+
+kotlinc $(find . -name "*.kt") -include-runtime -d main.jar
+java -jar main.jar
+
+
+Windows (powershell):
+
+kotlinc (Get-ChildItem -Path . -Recurse -Filter "*.kt").FullName -include-runtime -d output.jar
+java -jar main.jar
+
+************************************************************************/ 
 
 
 
