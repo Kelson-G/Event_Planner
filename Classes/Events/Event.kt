d7df36aa8268dc975d311c69b6c4592efe8463e1
g3nke11 <gisaack@byui.edu>
2025-01-31 10:23:44 -0700

    Event comments

--- Classes/Events/Event.kt
+++ Classes/Events/Event.kt
@@ -7,14 +7,17 @@ open class Event (
     var _address: String) {
     private var _tasks = mutableListOf<Task>()
 
+    // Function to add a task to the tasks list. No return variable
     fun addTask(task: Task) {
         _tasks.add(task)
     }
 
+    // Function to return the entire list of tasks
     fun getTasks(): List<Task> {
         return _tasks
     }
 
+    // Function to return the basic event details as a string list
     fun getDetails(): List<String> {
         val details = mutableListOf(_title, _description, _date, _time, _address)
         return details
