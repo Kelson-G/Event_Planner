67d400371e9657f4a418c71201b7889f6afc8cee
Kelson-G <kelson.gneiting@gmail.com>
2025-02-04 00:24:38 -0700

    Reorganizing and cleaning

--- Main/Classes/Actions/templatebuilder.kt
+++
@@ -1,9 +0,0 @@
-package actions
-
-// Uses a class to create a Template for the event to send to people
-// (Optional: Probably also reads a csv file to return an email list)
-// (Optional: Template in PDF?)
-
-class MyClass {
-    var height = "6ft"
-}
